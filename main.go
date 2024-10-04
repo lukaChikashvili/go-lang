c523c7a983b9e67505cd8eb6828e958752663413
luka chikashvili <lukachikashvili2020@gmail.com>
2024-10-04 15:30:44 +0400

    learn about arrays in go

--- main.go
+++ main.go
@@ -3,13 +3,43 @@ import "fmt"
 
 func main() {
 
-	var userName string
-	var age int
+	// define variables
+     var firstName string
+	 var lastName string
+	 var email string
+	 var userTickets uint
 
-	 fmt.Println("enter your name")
-	 fmt.Scan(&userName)
-	 fmt.Println("enter your age")
-	 fmt.Scan(&age)
-	 fmt.Println("your name is ", userName, "and you are ", age, "years old")
+	 var remainingTickets uint = 50
+	 var bookings [50]string
+
+
+// ask user to enter info
+  fmt.Println("Enter your first name")
+  fmt.Scan(&firstName)
+
+  fmt.Println("Enter your last name")
+  fmt.Scan(&lastName)
+
+  fmt.Println("Enter your email ")
+  fmt.Scan(&email)
+  
+  fmt.Println("Enter your tickets ")
+  fmt.Scan(&userTickets)
+
+  remainingTickets = remainingTickets - userTickets
+   bookings[0] = firstName + " " + lastName
+
+   fmt.Println("thank you", firstName, " ", lastName, "i will write on email", email)
+   fmt.Println(remainingTickets, "tickets are remained")
+   fmt.Println(bookings)
+   
+
+ 
+
+
+
+
+
+	
 	 
 }
