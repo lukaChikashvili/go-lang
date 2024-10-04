df36ed0dce47b7d220d16f77ef765e1a09dbad21
luka chikashvili <lukachikashvili2020@gmail.com>
2024-10-04 15:13:11 +0400

    learn about pointers

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
-
+import "fmt"
 
 func main() {
-   
 
+	var userName string
+	var age int
+
+	 fmt.Println("enter your name")
+	 fmt.Scan(&userName)
+	 fmt.Println("enter your age")
+	 fmt.Scan(&age)
+	 fmt.Println("your name is ", userName, "and you are ", age, "years old")
+	 
 }
