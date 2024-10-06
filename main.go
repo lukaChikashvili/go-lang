a463bb274145a7d716a49d0e911731f84716e7a1
luka chikashvili <lukachikashvili2020@gmail.com>
2024-10-06 13:57:08 +0400

    learn about conditional statements in go

--- main.go
+++ main.go
@@ -3,40 +3,22 @@ import "fmt"
 
 func main() {
 
-	// define variables
-     var firstName string
-	 var lastName string
-	 var email string
-	 var userTickets uint
-
-	 var remainingTickets uint = 50
-	 var bookings [50]string
-
-
-// ask user to enter info
-  fmt.Println("Enter your first name")
-  fmt.Scan(&firstName)
-
-  fmt.Println("Enter your last name")
-  fmt.Scan(&lastName)
-
-  fmt.Println("Enter your email ")
-  fmt.Scan(&email)
-  
-  fmt.Println("Enter your tickets ")
-  fmt.Scan(&userTickets)
-
-  remainingTickets = remainingTickets - userTickets
-   bookings[0] = firstName + " " + lastName
-
-   fmt.Println("thank you", firstName, " ", lastName, "i will write on email", email)
-   fmt.Println(remainingTickets, "tickets are remained")
-   fmt.Println(bookings)
+   fmt.Println("welcome to the quiz")
    
+   fmt.Println("enter your age")
+   var age uint
+   fmt.Scan(&age)
 
- 
+    if age >= 10 {
+      fmt.Println("you can play")
+    }else {
+      fmt.Println("you can not play")
+      return
+    }
 
 
+  
+    
 
 
 
