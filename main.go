100090dad33929c766a949a6fa2b2032d943f38a
luka chikashvili <lukachikashvili2020@gmail.com>
2024-10-06 14:08:18 +0400

    learn about functions in go

--- main.go
+++ main.go
@@ -1,27 +1,16 @@
 package main
 import "fmt"
 
+  func calculate(num1 int, num2 int) int {
+    return num1 + num2
+  }
+
+
 func main() {
-
-   fmt.Println("welcome to the quiz")
-   
-   fmt.Println("enter your age")
-   var age uint
-   fmt.Scan(&age)
-
-    if age >= 10 {
-      fmt.Println("you can play")
-    }else {
-      fmt.Println("you can not play")
-      return
-    }
-
-
   
-    
-
-
-
+  fmt.Println(calculate(10, 20))
+  
+  
 	
 	 
 }
